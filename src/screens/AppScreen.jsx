c8268d53d249fad8f4744fc72ec73cc373cc109d
clintjeff2 <clintjeff2@gmail.com>
2024-04-12 10:08:42 +0100

    Allowed the children of the route page to show via outlet

--- src/screens/AppScreen.jsx
+++ src/screens/AppScreen.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
 function AppScreen() {
 	return (
 		<>
 			<NavBar />
-			{/* <Outlet /> */}
+			<Outlet />
 		</>
 	);
 }
