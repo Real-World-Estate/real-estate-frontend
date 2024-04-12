3bb9c10bb22e389cc03d8d0c56fb0b4dcc7eef43
clintjeff2 <clintjeff2@gmail.com>
2024-04-12 10:44:40 +0100

    Added the Partners component tot he Home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Partners from '../components/Partners';
 
 function Home() {
 	return (
 		<div>
 			<Header />
+			<Partners />
 		</div>
 	);
 }
