0a94e0c810a6507e44b88b9d30806e52c6fd1a60
clintjeff2 <clintjeff2@gmail.com>
2024-04-12 16:39:49 +0100

    Added to the Home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Partners from '../components/Partners';
 import About from '../components/About';
+import Services from '../components/Services';
 
 function Home() {
 	return (
@@ -9,6 +10,7 @@ function Home() {
 			<Header />
 			<Partners />
 			<About />
+			<Services />
 		</div>
 	);
 }
