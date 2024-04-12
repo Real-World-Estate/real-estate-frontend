8c145d763018695a9235a2c747fe01a1b10318ad
clintjeff2 <clintjeff2@gmail.com>
2024-04-12 15:36:57 +0100

    Added the about section to the Home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Partners from '../components/Partners';
+import About from '../components/About';
 
 function Home() {
 	return (
 		<div>
 			<Header />
 			<Partners />
+			<About />
 		</div>
 	);
 }
