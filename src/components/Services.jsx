b0e2856caa770970987457fce1e3b890317deffe
clintjeff2 <clintjeff2@gmail.com>
2024-04-12 16:39:30 +0100

    Created the service provided section and styled it approprietly

---
+++ src/components/Services.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { vector } from '../assets';
+
+function Services() {
+	return (
+		<div className="service mg-top-lg">
+			<div className="title mg-top-lg">
+				<h2>
+					<span>Services We</span> provide
+				</h2>
+				<p className="mg-top">
+					Features which will build your trust back on Real Estate
+				</p>
+			</div>
+			<div className="content mg-top-lg">
+				<div className="item">
+					<img src={vector.vectorMotel} alt="Service content" />
+					<h4>Reasonable Apartments</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHouse} alt="Service content" />
+					<h4>Home and Farm Houses</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHotel} alt="Service content" />
+					<h4>Market and Business Center</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorMotel} alt="Service content" />
+					<h4>Reasonable Apartments</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHouse} alt="Service content" />
+					<h4>Home and Farm Houses</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHotel} alt="Service content" />
+					<h4>Market and Business Center</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHouse} alt="Service content" />
+					<h4>Home and Farm Houses</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+				<div className="item">
+					<img src={vector.vectorHotel} alt="Service content" />
+					<h4>Market and Business Center</h4>
+					<p className="text">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, non
+						at? Mollitia, officia! Quam sapiente ducimus culpa dolore quo
+						doloremque.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Services;
