f8c72ce34d6000f1d693f1ce0f092da237ab2de9
clintjeff2 <clintjeff2@gmail.com>
2024-04-14 20:19:51 +0100

    Wrote the markup for the Faqs section and styled it appropriately

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { faqs } from '../assets';
+import Question from './Question';
+
+function Faqs() {
+	return (
+		<div className="faqs">
+			<div className="title">
+				<img src={faqs.faqsMain} alt="Faqs Icon" />
+				<h4>Frequently Asked Questions</h4>
+			</div>
+			<div className="faqs-content mg-top-lg">
+				{Array.from({ length: 7 }).map((_, index) => (
+					<Question />
+				))}
+			</div>
+			<img src={faqs.iconBrownDown} alt="down icon" className="icon-down" />
+			<img src={faqs.iconBrownUp} alt="down icon" className="icon-up" />
+		</div>
+	);
+}
+
+export default Faqs;
