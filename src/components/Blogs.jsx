c6b938ebc1b4d775095e568bbb8fe9706169d41d
clintjeff2 <clintjeff2@gmail.com>
2024-04-15 03:02:56 +0100

    Created the markup for the blog section and styled it

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { image } from '../assets';
+
+function Blogs() {
+	return (
+		<div className="blog">
+			<h2>Blogs</h2>
+			<div className="blog-section mg-top-lg">
+				<div className="main">
+					<div className="main-item">
+						<img src={image.housePic1} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+					<div className="main-item">
+						<img src={image.housePic2} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+					<div className="main-item">
+						<img src={image.housePic3} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+					<div className="main-item">
+						<img src={image.housePic4} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+					<div className="main-item">
+						<img src={image.housePic5} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+					<div className="main-item">
+						<img src={image.houseHeader} alt="House Pics" />
+						<h3>Blog post that make businesses go really viral for all ages</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+							corporis reiciendis iste debitis quod est illo earum quaerat
+							soluta animi.
+						</p>
+						<button className="btn btn-secondary">Read More...</button>
+					</div>
+				</div>
+				<div className="side">
+					<div className="side-item">
+						<img src={image.houseHeader} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+					<div className="side-item">
+						<img src={image.housePic5} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+					<div className="side-item">
+						<img src={image.housePic4} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+					<div className="side-item">
+						<img src={image.housePic3} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+					<div className="side-item">
+						<img src={image.housePic2} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+					<div className="side-item">
+						<img src={image.housePic1} alt="House Pics" />
+						<div className="content">
+							<h3>Blog post that make businesses go...</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
+							</p>
+							<button className="btn btn-secondary">Read More...</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Blogs;
