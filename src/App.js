2d0e7e2fac9f9ce7084d757a3859a9f50ac1bd37
Vanessa Fuangi <vanessafuangi@gmail.com>
2024-04-16 09:43:37 +0100

    Set up to start the dashboard screen - route, stylesheet and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './screens/Home';
-import Blog from './screens/Blog';
-import Error404 from './screens/Error404';
-import AppScreen from './screens/AppScreen';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Blog from "./screens/Blog";
+import Error404 from "./screens/Error404";
+import AppScreen from "./screens/AppScreen";
+import Dashboard from "./screens/Dashboard";
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<AppScreen />}>
-						<Route path="" element={<Home />} />
-						<Route path="blog" element={<Blog />} />
-						<Route path="*" element={<Error404 />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppScreen />}>
+            <Route path="" element={<Home />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="*" element={<Error404 />} />
+          </Route>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
