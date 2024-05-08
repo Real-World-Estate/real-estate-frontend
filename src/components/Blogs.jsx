0837a4db1704963fb4eaa9c350cc1e974fdf695d
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-05-08 11:45:35 +0100

    Added real world data to the blog section and changed a text on the feedback section

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,138 +1,146 @@
-import React from 'react';
-import { image } from '../assets';
+import React from "react";
+import { image, pictures } from "../assets";
 
 function Blogs() {
-	return (
-		<div className="blog" id='blog'>
-			<h2>Blogs</h2>
-			<div className="blog-section mg-top-lg">
-				<div className="main">
-					<div className="main-item">
-						<img src={image.housePic1} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-					<div className="main-item">
-						<img src={image.housePic2} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-					<div className="main-item">
-						<img src={image.housePic3} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-					<div className="main-item">
-						<img src={image.housePic4} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-					<div className="main-item">
-						<img src={image.housePic5} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-					<div className="main-item">
-						<img src={image.houseHeader} alt="House Pics" />
-						<h3>Blog post that make businesses go really viral for all ages</h3>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-							corporis reiciendis iste debitis quod est illo earum quaerat
-							soluta animi.
-						</p>
-						<button className="btn btn-secondary">Read More...</button>
-					</div>
-				</div>
-				<div className="side">
-					<div className="side-item">
-						<img src={image.houseHeader} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-					<div className="side-item">
-						<img src={image.housePic5} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-					<div className="side-item">
-						<img src={image.housePic4} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-					<div className="side-item">
-						<img src={image.housePic3} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-					<div className="side-item">
-						<img src={image.housePic2} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-					<div className="side-item">
-						<img src={image.housePic1} alt="House Pics" />
-						<div className="content">
-							<h3>Blog post that make businesses go...</h3>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum...
-							</p>
-							<button className="btn btn-secondary">Read More...</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="blog" id="blog">
+      <h2>Blogs</h2>
+      <div className="blog-section mg-top-lg">
+        <div className="main">
+          <div className="main-item">
+            <img src={image.housePic1} alt="House Pics" />
+            <h3>
+              House Hunting in a Hot Market? Here's How to Stay Cool (and
+              Competitive)!
+            </h3>
+            <p>
+              The real estate market is booming! While that means a lot of great
+              options for potential buyers, it can also feel overwhelming,
+              especially for first-time homebuyers.
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+          <div className="main-item">
+            <img src={image.housePic2} alt="House Pics" />
+            <h3>
+              Rising prices and lightning-fast sales can leave you feeling
+              discouraged.
+            </h3>
+            <p>
+              But fear not, house hunters! This post will equip you with the
+              knowledge and strategies you need to navigate a hot market and
+              find your dream home
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+          <div className="main-item">
+            <img src={image.manPic1} alt="House Pics" />
+            <h3> Breathe Easy and Live Clean with Competent Property</h3>
+            <p>
+              It's about creating a healthy and comfortable environment for you
+              and your loved ones, free from allergens, pests, and the lingering
+              effects of dust.
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+          <div className="main-item">
+            <img src={image.manPic2} alt="House Pics" />
+            <h3>The power of proactive pest control</h3>
+            <p>
+              Discover how fumigation can prevent infestations before they
+              start, allowing you to enjoy your home pest-free all year round.
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+          <div className="main-item">
+            <img src={pictures.detergentLab} alt="House Pics" />
+            <h3>The magic of stain removal</h3>
+            <p>
+              Learn how our powerful stain-fighting formulas tackle even the
+              toughest messes, leaving your clothes looking spotless.
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+          <div className="main-item">
+            <img src={pictures.detergents} alt="House Pics" />
+            <h3>Color protection redefined</h3>
+            <p>
+              Discover how our detergents use cutting-edge technology to keep
+              your clothes vibrant wash after wash, preventing fading and
+              dullness.
+            </p>
+            <button className="btn btn-secondary">Read More...</button>
+          </div>
+        </div>
+        <div className="side">
+          <div className="side-item">
+            <img src={image.houseHeader} alt="House Pics" />
+            <div className="content">
+              <h3>Unique properties...</h3>
+              <p>
+                Discover a diverse range of houses waiting to be transformed
+                into your dream home
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+          <div className="side-item">
+            <img src={image.housePic5} alt="House Pics" />
+            <div className="content">
+              <h3>Expertise you can trust...</h3>
+              <p>
+                Our team of local real estate professionals is here to guide you
+                every step of the way.
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+          <div className="side-item">
+            <img src={pictures.cleaner} alt="House Pics" />
+            <div className="content">
+              <h3>The convenience and expertise of professional services...</h3>
+              <p>
+                Take the stress out of cleaning and let our experienced
+                technicians handle the tough jobs.
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+          <div className="side-item">
+            <img src={pictures.fumigationPic} alt="House Pics" />
+            <div className="content">
+              <h3>Whether you're looking for a one-time deep clean or...</h3>
+              <p>
+                Competent Property is here to help you breathe easy and live
+                clean this spring
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+          <div className="side-item">
+            <img src={pictures.detergentSpray} alt="House Pics" />
+            <div className="content">
+              <h3>The science of long-lasting freshness...</h3>
+              <p>
+                Explore the science behind our long-lasting fragrance technology
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+          <div className="side-item">
+            <img src={pictures.soapInCompany} alt="House Pics" />
+            <div className="content">
+              <h3>Laundry day...</h3>
+              <p>
+                But what if laundry day could be a breeze? we believe it can!
+              </p>
+              <button className="btn btn-secondary">Read More...</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Blogs;
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,64 +1,64 @@
-import React from 'react';
-import { profile } from '../assets';
+import React from "react";
+import { profile } from "../assets";
 
 function Feedback() {
-	return (
-		<div className="feedback" id="testimonials">
-			<div className="title">
-				<span></span>
-				<h2>Our Customer Feedback</h2>
-				<span></span>
-			</div>
-			<div className="feedback-container mg-top-lg">
-				<div className="item">
-					<div className="head">
-						<img src={profile.profile1} alt="Profile pics" />
-						<div className="profile">
-							<h5>Vanzy Theodore</h5>
-							<span>Timely And Proffessional Sellers</span>
-						</div>
-					</div>
-					<p>
-						Absolutely Thrilled With The Seamleass Experience Using Real Estate
-						Competent Property. Found M Dream Home Faster than I ever Imagined.
-						The Intuitive Interface Made Browsing Listing A Breeze. Highly
-						Recommend!
-					</p>
-				</div>
-				<div className="item">
-					<div className="head">
-						<img src={profile.profile2} alt="Profile pics" />
-						<div className="profile">
-							<h5>Hyzeah A. Miriam</h5>
-							<span>Cheam And Comfortable</span>
-						</div>
-					</div>
-					<p>
-						Peace Of Mind For You, Independence For Them. They'll Have The
-						Freedom! Found Exactly What I was Looking For In No Time. The
-						Detailed Property Descriptions And Photos Game Me A Clear Picture Of
-						Each Listing. Thanks To Real Estate Competent Property, I'm Now
-						Happily Settled In My New Home.
-					</p>
-				</div>
-				<div className="item">
-					<div className="head">
-						<img src={profile.profile4} alt="Profile pics" />
-						<div className="profile">
-							<h5>Eve Shelta</h5>
-							<span>Timely And Proffessional Agents</span>
-						</div>
-					</div>
-					<p>
-						I Can't Thank Real Estate Competent Property Enough For Helping Me
-						Find The Perfect Apartment. The App's Search Filters Saved Me So
-						Much Time, And The Customer Support Was Exceptional. Moving Has
-						Never Been Easier!
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="feedback" id="testimonials">
+      <div className="title">
+        <span></span>
+        <h2>Our Customer Feedback</h2>
+        <span></span>
+      </div>
+      <div className="feedback-container mg-top-lg">
+        <div className="item">
+          <div className="head">
+            <img src={profile.profile1} alt="Profile pics" />
+            <div className="profile">
+              <h5>Vanzy Theodore</h5>
+              <span>Timely And Proffessional Sellers</span>
+            </div>
+          </div>
+          <p>
+            Absolutely Thrilled With The Seamleass Experience Using Real Estate
+            Competent Property. Found M Dream Home Faster than I ever Imagined.
+            The Intuitive Interface Made Browsing Listing A Breeze. Highly
+            Recommend!
+          </p>
+        </div>
+        <div className="item">
+          <div className="head">
+            <img src={profile.profile2} alt="Profile pics" />
+            <div className="profile">
+              <h5>Hyzeah A. Miriam</h5>
+              <span>Cheap And Comfortable</span>
+            </div>
+          </div>
+          <p>
+            Peace Of Mind For You, Independence For Them. They'll Have The
+            Freedom! Found Exactly What I was Looking For In No Time. The
+            Detailed Property Descriptions And Photos Game Me A Clear Picture Of
+            Each Listing. Thanks To Real Estate Competent Property, I'm Now
+            Happily Settled In My New Home.
+          </p>
+        </div>
+        <div className="item">
+          <div className="head">
+            <img src={profile.profile4} alt="Profile pics" />
+            <div className="profile">
+              <h5>Eve Shelta</h5>
+              <span>Timely And Proffessional Agents</span>
+            </div>
+          </div>
+          <p>
+            I Can't Thank Real Estate Competent Property Enough For Helping Me
+            Find The Perfect Apartment. The App's Search Filters Saved Me So
+            Much Time, And The Customer Support Was Exceptional. Moving Has
+            Never Been Easier!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feedback;
